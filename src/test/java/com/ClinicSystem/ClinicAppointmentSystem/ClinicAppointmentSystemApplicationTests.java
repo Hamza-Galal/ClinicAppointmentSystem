32c9a03b5e4f515582facc75ca6a948dbcbc3b07
Hamza-Galal <hamza.galal107@gmail.com>
2026-09-01 22:26:37 +0300

    Initial Commit

---
+++ src/test/java/com/ClinicSystem/ClinicAppointmentSystem/ClinicAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClinicSystem.ClinicAppointmentSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
